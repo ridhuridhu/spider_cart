ccb082d4d574c6abca969d3187fdeb49ffb4f949
ridhuridhu <ridhuridhu05@gmail.com>
2020-09-03 13:24:01 +0530

    password validation added

--- public/scripts/error.js
+++ public/scripts/error.js
@@ -1,5 +1,35 @@
 let passwordBox=document.getElementById("password");
-
+let submitBtn=document.getElementById("submitBtn");
+let passValid=document.getElementById("passValid");
+passValid.style.display="none";
+submitBtn.style.cursor="none";
 passwordBox.addEventListener("keyup",()=>{
-    console.log(passwordBox.value);
+    let p=(passwordBox.value);
+    passValid.style.display="block";
+    let valid=false;
+    passValid.innerHTML="";
+    if(p.length<4){
+        passValid.innerHTML+="<li>Password must be at least 4 characters! </li>";
+    }
+    else if(!p.match(/[a-z]/)){
+        passValid.innerHTML+="<li>Password must contain at least one lower case letter </li>";
+    }
+    else if(!p.match(/[A-Z]/)){
+        passValid.innerHTML+="<li>Password must contain at least one Upper case letter </li>";
+    }
+    else{
+        valid=true;
+        passValid.style.display="none";
+    }
+
+    //console.log(testString(p));
+
+    if(valid){
+        submitBtn.classList.remove("disabled");
+        submitBtn.style.cursor="pointer";
+
+    }else{
+        submitBtn.classList.add("disabled");
+        submitBtn.style.cursor="none";
+    }
 });
